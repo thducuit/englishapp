4257aa05e3cb0a06ae3d74e63cf929cdf6aaa13b
thducuit <thducuit@gmail.com>
2016-11-24 08:45:46 +0000

    fix path file

--- app/models/audio.rb
+++ app/models/audio.rb
@@ -1,6 +1,8 @@
 class Audio < ActiveRecord::Base
     
-    has_attached_file :file, { validate_media_type: false }
+    has_attached_file :file, { validate_media_type: false }, 
+                      :url  => "/assets/audios/:id/:style/:basename.:extension",
+                      :path => ":rails_root/public/assets/audios/:id/:style/:basename.:extension"
     
     
     validates_attachment_content_type :file,  content_type: /\Aaudio\/.*\Z/
